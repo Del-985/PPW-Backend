e0be94b741bd7ce7ce54c7f78644a8b6a4a34bf1
Del-985 <119140366+Del-985@users.noreply.github.com>
2025-07-01 20:02:54 -0500

    Update businessController.js

--- controllers/businessController.js
+++ controllers/businessController.js
@@ -1,5 +1,5 @@
 const pool = require('../config/db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const registerBusinessUser = async (req, res) => {
   const { business_name, email, password } = req.body;
