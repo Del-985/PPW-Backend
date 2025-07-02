6fbd5dbf6f5b72653d1085f8c566fa9b0f746178
Del-985 <119140366+Del-985@users.noreply.github.com>
2025-07-02 16:48:28 -0500

    Update businessRoutes.js

--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { registerBusinessUser } = require('../controllers/businessController');
 const { loginBusinessUser } = require('../controllers/businessController');
+const { registerBusinessUser, loginBusinessUser, getBusinessContacts } = require('../controllers/businessController');
 
 
 router.post('/register', registerBusinessUser);
 router.post('/login', loginBusinessUser);
+router.get('/contacts', getBusinessContacts);
 
 
 module.exports = router;
