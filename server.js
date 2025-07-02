279d3c8f3980774bc65953eee9a37c8d67e380d1
Del-985 <119140366+Del-985@users.noreply.github.com>
2025-07-01 19:38:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ app.use(express.json());
 // Routes
 const contactRoutes = require('./routes/contactRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+const businessRoutes = require('./routes/businessRoutes');
 
 app.use('/api', contactRoutes);
 app.use('/api/admin', adminRoutes);
+app.use('/api/business', businessRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
