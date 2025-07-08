18bc2b6e6928944956751405cfc13feb9abde481
Del-985 <119140366+Del-985@users.noreply.github.com>
2025-07-08 18:52:54 -0500

    Delete routes/scheduleRoutes.js

--- routes/scheduleRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const scheduleController = require('../controllers/scheduleController');
-
-router.post('/', auth, scheduleController.createTask);
-router.get('/', auth, scheduleController.getTasks);
-
-module.exports = router;
