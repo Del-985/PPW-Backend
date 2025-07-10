faf5ce2e3748db1fe3d556cfbcc897aeda62d003
Del-985 <119140366+Del-985@users.noreply.github.com>
2025-07-10 00:14:42 -0500

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {
-  getAllContacts,
+  getAllContacts, 
   getAllBusinessUsers,
   deleteBusinessUser,
   deleteContact,
