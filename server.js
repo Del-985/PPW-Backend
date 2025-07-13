209b8367770d92a49b1dadc35bd135e742398b23
Del-985 <119140366+Del-985@users.noreply.github.com>
2025-07-13 11:25:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const PDFDocument = require('pdfkit');
 require('dotenv').config();
 
 const app = express();
