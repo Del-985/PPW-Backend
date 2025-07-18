ecb3992d71a57caa9397feaad1773e438d93a893
Del-985 <119140366+Del-985@users.noreply.github.com>
2025-07-17 19:11:38 -0500

    Update quoteRoutes.js

--- routes/quoteRoutes.js
+++ routes/quoteRoutes.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const pool = require('../db');
 const nodemailer = require('nodemailer');
 const path = require('path');
 const fs = require('fs');
